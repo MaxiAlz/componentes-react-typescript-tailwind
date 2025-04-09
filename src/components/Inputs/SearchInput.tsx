5caf4fec5dc787e718e2ca34ff09be0ef195446b
maxiAlz <maxialz99@hotmail.com>
2025-04-09 11:29:57 -0300

    modifico search input con propiedades heredadas

--- src/components/Inputs/SearchInput.tsx
+++ src/components/Inputs/SearchInput.tsx
@@ -1,48 +1,63 @@
 import { MdSearch } from 'react-icons/md';
 
-interface SearchInputProps {
-  onSubmit?: () => void | undefined;
-  nameInput: string;
-  placeholderInput: string;
-  labelButton: string;
-  titleInput: string;
-  icon?: React.FC<React.SVGProps<SVGSVGElement>> | undefined;
+interface SearchInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  onSubmit?: () => void;
+  labelButton?: string;
+  titleInput?: string;
+  icon?: React.FC<React.SVGProps<SVGSVGElement>>;
+  className?: string;
+  buttonProps?: React.ButtonHTMLAttributes<HTMLButtonElement>; // Props para el botón
 }
 
 const SearchInput = ({
   onSubmit,
-  nameInput,
-  placeholderInput = 'Ingrese un texto',
+  name,
+  placeholder = 'Ingrese un texto',
   labelButton = 'Buscar',
   titleInput = 'Ingrese un texto',
   icon: Icon = MdSearch,
+  className,
+  buttonProps,
+  ...inputProps
 }: SearchInputProps) => {
   return (
-    <div className='m-4 flex w-full flex-col items-center justify-center rounded-lg border border-solid p-4'>
-      <div className='text-center text-parragraph dark:text-white'>
-        <p>{titleInput}</p>
+    <div
+      className={`flex flex-col rounded-lg bg-white dark:border-boxdark dark:bg-boxdark ${className}`}
+    >
+      <div className='text-start text-parragraph dark:text-white'>
+        <p className='ml-1'>{titleInput}</p>
       </div>
-      <form onSubmit={onSubmit} className='w-full'>
-        <div className='relative m-2 w-full'>
-          {Icon && (
-            <span className='absolute left-3 top-1/2 -translate-y-1/2 text-gray-500'>
-              <Icon className='size-5' />
-            </span>
-          )}
-          <input
-            type='text'
-            name={nameInput}
-            placeholder={placeholderInput}
-            className={`w-full rounded-lg border p-2 pl-10`}
-          />
-        </div>
-        <button
-          type='submit'
-          className='m-2 w-full rounded-full bg-info p-2 font-semibold text-white hover:bg-darknessPrimary'
+      <div className='md:w-1/2'>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            onSubmit?.();
+          }}
+          className='flex w-full'
         >
-          {labelButton}
-        </button>
-      </form>
+          <div className='relative my-2 w-full'>
+            {Icon && (
+              <span className='absolute left-3 top-1/2 -translate-y-1/2 text-gray-500'>
+                <Icon className='size-5' />
+              </span>
+            )}
+            <input
+              type='text'
+              name={name}
+              placeholder={placeholder}
+              className='w-full rounded-lg border p-2 pl-10 dark:bg-boxdark dark:text-white'
+              {...inputProps}
+            />
+          </div>
+          <button
+            type='button'
+            className='my-2 ml-2 rounded-md bg-info p-2 px-4 font-semibold text-white hover:bg-darknessPrimary'
+            {...buttonProps}
+          >
+            {labelButton}
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
